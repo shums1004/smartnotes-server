a6e7a1b685d2ef92f159d6c2e0346f5b5cb773dc
shums1004 <shums1004@gmail.com>
2025-05-02 22:17:38 -0700

    Handeling Cors

--- config.js
+++ config.js
@@ -6,4 +6,4 @@ export const jwtSecret = process.env.JWT_SECRET || "your_jwt_secret";
 export const mongoURL = process.env.MONGO_URL
 export const openAIKey = process.env.OPENAI_API_KEY || "your_openai_api_key";
 export const port = process.env.PORT || 5000;
-export const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
+export const corsOrigin = process.env.CORS_ORIGIN;
