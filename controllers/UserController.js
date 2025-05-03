118ecc54a8aeb0124943f76b8acb7a7a120a309e
shums1004 <patil.shub@northeastern.edu>
2025-05-03 01:22:13 -0700

    changed cookie settings

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -40,8 +40,8 @@ export const login = async (req, res) =>{
         
         res.cookie('token', token, {
             httpOnly:true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'Strict',
+            secure: true,
+            sameSite: 'None',
         }).status(200)
         .json({message : 'Login successful', username : user.username});
     }   
